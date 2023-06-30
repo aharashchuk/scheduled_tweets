b0516f3678468523a17fe37e049b1caa19b49df9
aharashchuk <a.harashchuk@gmail.com>
2023-06-30 22:06:38 +0200

    flash alerts

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,8 @@
 class MainController < ApplicationController
   def index
     @title = 'Main page'
+    flash.now[:notice] = 'Logged in successfully!'
+    flash.now[:alert] = 'Invalid email or password!'
   end
     
 end
