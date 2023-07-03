d086b856c989339a34f3ce1e58fc64e9daa44cc3
aharashchuk <a.harashchuk@gmail.com>
2023-07-03 23:44:48 +0200

    sign up start

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 Rails.application.routes.draw do
   get "/about-us", to: "about#index", as: :about
   get "/", to: "main#index", as: :root
+  get "sign_up", to: "registrations#new"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
 end
