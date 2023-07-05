1e037aa1b7481a400a15bc02c260f36ccc6c1d67
aharashchuk <a.harashchuk@gmail.com>
2023-07-05 22:01:46 +0200

    sign up store

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 
 Rails.application.routes.draw do
   get "/about-us", to: "about#index", as: :about
+  
   get "/", to: "main#index", as: :root
+
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
 end
