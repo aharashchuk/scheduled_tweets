f3d9d867f0f4f8070272775c47381ac24b82e09f
aharashchuk <a.harashchuk@gmail.com>
2023-07-19 21:27:12 +0200

    logout implemented

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,8 +1,7 @@
 class MainController < ApplicationController
   def index
-    @title = 'Main page'
-    flash.now[:notice] = 'Logged in successfully!'
-    flash.now[:alert] = 'Invalid email or password!'
-  end
-    
+    if session[:user_id]
+      @user = User.find_by(id: session[:user_id])
+    end    
+  end    
 end
