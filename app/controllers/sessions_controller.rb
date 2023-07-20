2c2d6692bef39c89134487b0d0a80eab6a54534c
aharashchuk <a.harashchuk@gmail.com>
2023-07-20 23:12:00 +0200

    sign in added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,21 @@
 class SessionsController < ApplicationController
-    def destroy
-        session[:user_id] = nil
-        redirect_to root_path, notice: "You have been logged out"
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user.present? && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "You have been logged in"
+    else
+      flash.now[:alert] = "Invalid email or password"
+      render :new, notice: "Invalid email or password"
     end
+
+  end
+    
+  def destroy
+      session[:user_id] = nil
+      redirect_to root_path, notice: "You have been logged out"
+  end
 end
