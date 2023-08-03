6be40614c7cb9f35c8b875a282e63b4789851ea5
aharashchuk <a.harashchuk@gmail.com>
2023-08-03 23:18:36 +0200

    twitter omniauth added

---
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -0,0 +1,5 @@
+class OmniauthCallbacksController < ApplicationController
+  def twitter
+    render plain: "TODO: Twitter callback"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,8 @@ Rails.application.routes.draw do
   post "password/reset", to: "password_resets#create"
   get "password/reset/edit", to: "password_resets#edit"
   patch "password/reset/edit", to: "password_resets#update"
+
+  get "auth/twitter/callback", to: "omniauth_callbacks#twitter"
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
